dcedf891ae9d00cbfa5f4ec7b11772087c33fe25
Lolisuki18 <leninh2004@gmail.com>
2025-05-20 13:05:39 +0700

    new

---
+++ StringInteraction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8; // cho mình bấm tiếng Việt có dấu
+String ho = "Nguyễn";
+String tenlot = "Thị";
+String ten = "Tủn";
+
+string full_name = ho +" " + tenlot + " " + ten; //cách 1 // cứ 1 "+" là thêm 1 ô nhớ  -> dùng cái này tốn ô nhớ
+Console.WriteLine(full_name);
+
+StringBuilder sb = new StringBuilder(); //cách 2
+sb.Append(ho); //thêm vào cuối chuỗi
+sb.Append(" ");
+sb.Append(tenlot);
+sb.Append(" ");
+sb.Append(ten);
+Console.WriteLine(sb.ToString()); //chuyển về string để in
